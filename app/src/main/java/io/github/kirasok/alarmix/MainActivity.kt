8dbb98a04d9eb9f0c1a2b28a570ece30307ae2ca
Kira Sokolova <Kyra256@proton.me>
2023-12-14 19:31:13 +0300

    refactor: use zoneddatetime for example

--- app/src/main/java/io/github/kirasok/alarmix/MainActivity.kt
+++ app/src/main/java/io/github/kirasok/alarmix/MainActivity.kt
@@ -13,6 +13,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Snackbar
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -22,81 +23,95 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import io.github.kirasok.alarmix.ui.theme.AlarmixTheme
-import java.time.Instant
-import java.time.LocalDateTime
-import java.time.ZoneId
+import java.time.ZonedDateTime
 
-
-private const val NOTIFICATION_ID = 1
 private const val PRIMARY_CHANNEL_ID = "primary_notification_channel"
 
 class MainActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        createNotificaionChannel(applicationContext.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager)
-        setContent {
-            AlarmixTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background,
-                ) {
-                    Alarm()
-                }
-            }
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    createNotificationChannel(applicationContext.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager)
+    setContent {
+      AlarmixTheme {
+        // A surface container using the 'background' color from the theme
+        Surface(
+          modifier = Modifier.fillMaxSize(),
+          color = MaterialTheme.colorScheme.background,
+        ) {
+          Alarm()
         }
+      }
     }
+  }
 }
 
 @Composable
 fun Alarm() {
-    val time = rememberSaveable {
-        mutableStateOf("")
-    }
-    val context = LocalContext.current
-    val alarmManager = context.getSystemService(Context.ALARM_SERVICE) as? AlarmManager
-    Column {
-        Text(text = time.value.toString())
-        ElevatedButton(onClick = {
-            val now = LocalDateTime.now()
-            val to = now.plusMinutes(4)
-            time.value = to.toString()
+  val time = rememberSaveable {
+    mutableStateOf("")
+  }
+  val showSnackbar = rememberSaveable {
+    mutableStateOf(false)
+  }
+  val context = LocalContext.current
+  val alarmManager = context.getSystemService(AlarmManager::class.java)
+  Column {
+    Text(text = time.value)
+    ElevatedButton(onClick = {
+      val now = ZonedDateTime.now()
+      val to = now.plusMinutes(1)
+      val alarm = io.github.kirasok.alarmix.domain.model.Alarm(
+        timestamp = to.toEpochMillisecond(),
+        enabled = true
+      )
+      time.value = to.toString()
 
-            val intent = Intent(context, AlarmReceiver::class.java)
-            intent.flags = Intent.FLAG_ACTIVITY_NO_ANIMATION
-            val pendingIntent = PendingIntent.getBroadcast(
-                context,
-                0, intent, PendingIntent.FLAG_IMMUTABLE
-            )
+      val pendingIntent = PendingIntent.getBroadcast(
+        context,
+        alarm.id,
+        Intent(context.applicationContext, AlarmReceiver::class.java),
+        PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
+      )
+      val openAppIntent = PendingIntent.getActivity(
+        context.applicationContext,
+        alarm.id,
+        Intent(context.applicationContext, MainActivity::class.java),
+        PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
+      )
 
-            val zoneinfo = ZoneId
-                .systemDefault()
-                .getRules()
-                .getOffset(
-                    Instant.now()
-                )
-            alarmManager!!.setAlarmClock(
-                AlarmManager.AlarmClockInfo(
-                    to.toEpochSecond(zoneinfo) * 1000,
-                    pendingIntent
-                ), pendingIntent
-            )
-        }) {
-            Text(text = "set time")
-        }
+      val alarmClockInfo = AlarmManager.AlarmClockInfo(alarm.timestamp, openAppIntent)
+      if (alarmManager.canScheduleExactAlarms()) {
+        alarmManager.setAlarmClock(
+          alarmClockInfo,
+          pendingIntent
+        )
+      } else {
+        showSnackbar.value = true
+      }
+
+    }) {
+      Text(text = "set time")
     }
+    if (showSnackbar.value) {
+      Snackbar {
+        Text(text = "Couldn't set alarm clock")
+      }
+    }
+  }
 }
 
-private fun createNotificaionChannel(notificationManager: NotificationManager) {
-    // Create the NotificationChannel with all the parameters.
-    val notificationChannel = NotificationChannel(
-        PRIMARY_CHANNEL_ID,
-        "Stand Up Notification",
-        NotificationManager.IMPORTANCE_DEFAULT
-    )
-    notificationChannel.enableLights(true)
-    notificationChannel.lightColor = Color.Red.value.toInt()
-    notificationChannel.enableVibration(true)
-    notificationChannel.description = "Notifies every 15 minutes to stand up and walk"
-    notificationManager.createNotificationChannel(notificationChannel)
+private fun ZonedDateTime.toEpochMillisecond(): Long = this.toEpochSecond() * 1000
+
+private fun createNotificationChannel(notificationManager: NotificationManager) {
+  // Create the NotificationChannel with all the parameters.
+  val notificationChannel = NotificationChannel(
+    PRIMARY_CHANNEL_ID,
+    "Stand Up Notification",
+    NotificationManager.IMPORTANCE_DEFAULT
+  )
+  notificationChannel.enableLights(true)
+  notificationChannel.lightColor = Color.Red.value.toInt()
+  notificationChannel.enableVibration(true)
+  notificationChannel.description = "Notifies every 15 minutes to stand up and walk"
+  notificationManager.createNotificationChannel(notificationChannel)
 }
