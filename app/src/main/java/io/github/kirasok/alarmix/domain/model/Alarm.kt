69c3c393ebe5801efcd95d4e6716c42daaa67e4d
Kira Sokolova <Kyra256@proton.me>
2024-03-19 17:18:59 +0300

    refactor(model): remove unnecessary fields

--- app/src/main/java/io/github/kirasok/alarmix/domain/model/Alarm.kt
+++ app/src/main/java/io/github/kirasok/alarmix/domain/model/Alarm.kt
@@ -1,28 +1,13 @@
 package io.github.kirasok.alarmix.domain.model
 
-import androidx.room.Embedded
 import androidx.room.Entity
 import androidx.room.PrimaryKey
-import java.util.UUID
+import java.time.ZonedDateTime
 
 @Entity
 data class Alarm(
   @PrimaryKey(autoGenerate = true) val id: Int = 0,
-  val timestamp: Long,
-  val enabled: Boolean,
-  val label: String = "",
-  @Embedded val days: Days = Days(),
-)
-
-@Entity
-data class Days(
-  val monday: Boolean = false,
-  val tuesday: Boolean = false,
-  val wednesday: Boolean = false,
-  val thursday: Boolean = false,
-  val friday: Boolean = false,
-  val saturday: Boolean = false,
-  val sunday: Boolean = false,
+  val timestamp: ZonedDateTime,
 )
 
 class InvalidAlarmException(message: String) : Exception(message)
