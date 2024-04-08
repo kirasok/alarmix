4b594fe1c5ba40f2624a5b594b72c82ba828fa32
Kira Sokolova <Kyra256@proton.me>
2024-04-08 09:01:45 +0300

    feat(ui): style time text

--- app/src/main/java/io/github/kirasok/alarmix/presentation/dismiss/DismissScreen.kt
+++ app/src/main/java/io/github/kirasok/alarmix/presentation/dismiss/DismissScreen.kt
@@ -5,9 +5,11 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.ElevatedButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -53,8 +55,11 @@ fun DismissScreen(navController: NavController, viewModel: DismissViewModel = hi
             DateTimeFormatter.ofLocalizedTime(
               FormatStyle.SHORT
             )
-          )
+          ),
+          style = MaterialTheme.typography.headlineLarge
         )
+
+        Spacer(modifier = Modifier.height(16.dp))
 
         Row(horizontalArrangement = Arrangement.Center) {
           ElevatedButton(onClick = { viewModel.onEvent(DismissEvent.DismissAlarm) }) {
