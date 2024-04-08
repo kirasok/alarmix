f3824777f68eee75881ece8598f961d05debc3c5
Kira Sokolova <Kyra256@proton.me>
2024-04-08 09:14:44 +0300

    fix(ui): fill max size of DismissScreen

--- app/src/main/java/io/github/kirasok/alarmix/presentation/dismiss/DismissScreen.kt
+++ app/src/main/java/io/github/kirasok/alarmix/presentation/dismiss/DismissScreen.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
@@ -51,7 +52,9 @@ fun DismissScreen(navController: NavController, viewModel: DismissViewModel = hi
 
   Scaffold {
     Column(
-      modifier = Modifier.padding(it),
+      modifier = Modifier
+        .fillMaxSize()
+        .padding(it),
       verticalArrangement = Arrangement.Center,
       horizontalAlignment = Alignment.CenterHorizontally
     ) {
